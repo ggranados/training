3f9e37d85080fc89d3dd14483f0fcf7c85556b63
Guillermo Granados <guillermo.granados@outlook.com>
2023-07-24 19:17:13 -0400

    [docs] updated reference validator

--- code/src/edu/datadrivendocs/code/utils/MarkdownFileReferences.java
+++ code/src/edu/datadrivendocs/code/utils/MarkdownFileReferences.java
@@ -8,7 +8,7 @@ public class MarkdownFileReferences {
 
     public static void main(String[] args) throws IOException {
         // Specify the path to the directory containing the markdown files
-        String path = "C:\\src\\data-driven-docs\\docs\\pages";
+        String path = "docs";
 
         // Get all distinct file names from nested markdown files
         Set<String> allMarkdownFiles = getAllMarkdownFiles(path);
@@ -21,19 +21,15 @@ public class MarkdownFileReferences {
         unreferencedFiles.removeAll(referencedFiles);
 
         // Display the results
-        System.out.println("All distinct markdown files:");
-        for (String file : allMarkdownFiles) {
-            System.out.println(file);
-        }
+        System.out.println("All distinct markdown files:" + allMarkdownFiles.size());
 
-        System.out.println("\nReferenced markdown files:");
-        for (String file : referencedFiles) {
-            System.out.println(file);
-        }
 
-        System.out.println("\nUnreferenced markdown files:");
+        System.out.println("\nReferenced markdown files:" + referencedFiles.size());
+
+
+        System.out.println("\nUnreferenced markdown files:" + unreferencedFiles.size());
         for (String file : unreferencedFiles) {
-            System.out.println(file);
+            System.out.println(" - " + file);
         }
     }
 
@@ -42,10 +38,10 @@ public class MarkdownFileReferences {
         Set<String> markdownFiles = new HashSet<>();
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path))) {
             for (Path entry : stream) {
-                if (Files.isDirectory(entry)) {
+                if (Files.isDirectory(entry) && !entry.getFileName().toString().equals("common")) {
                     markdownFiles.addAll(getAllMarkdownFiles(entry.toString()));
                 }
-                if(entry.getFileName().toString().endsWith("md"))
+                if(entry.getFileName().toString().endsWith(".md"))
                     markdownFiles.add(entry.getFileName().toString());
             }
         }
@@ -55,20 +51,34 @@ public class MarkdownFileReferences {
     // Get all referenced file names from markdown files in a directory
     private static Set<String> getReferencedFiles(String path) throws IOException {
         Set<String> referencedFiles = new HashSet<>();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path), "*.md")) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path))) {
             for (Path entry : stream) {
-                List<String> lines = Files.readAllLines(entry);
-                for (String line : lines) {
-                    // Assuming that references to other markdown files follow the format "[filename]"
-                    int start = line.indexOf('[');
-                    int end = line.indexOf(']');
-                    if (start != -1 && end != -1 && end > start) {
-                        String referencedFile = line.substring(start + 1, end);
-                        referencedFiles.add(referencedFile);
+                if (Files.isDirectory(entry)) {
+                    referencedFiles.addAll(getReferencedFiles(entry.toString()));
+                }
+                if(entry.getFileName().toString().endsWith(".md")) {
+                    List<String> lines = Files.readAllLines(entry);
+                    for (String line : lines) {
+                        // Assuming that references to other markdown files follow the format "[filename]"
+                        int start = line.indexOf('(');
+                        int end = line.indexOf(')');
+                        if (start != -1 && end != -1 && end > start) {
+                            String referencedFile = line.substring(start + 1, end);
+                            if(referencedFile.endsWith(".md"))
+                                referencedFiles.add(getFileNameOnly(referencedFile));
+                        }
                     }
                 }
             }
         }
         return referencedFiles;
+    }
+
+    private static String getFileNameOnly(String path) {
+        // Get the last index of the forward slash '/' in the path
+        int lastSlashIndex = path.lastIndexOf('/');
+
+        // Extract the file name by using substring from lastSlashIndex + 1
+        return path.substring(lastSlashIndex + 1);
     }
 }
